9caae3b14fa458e71d3412fd79428d5276f26ea0
Kushagra Singh <kushagragnoida@gmail.com>
2024-12-23 07:28:35 +0530

    Routing Added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './components/homepage';
+import SignupPage from './components/SignupPage';
+import SignupPage2 from './components/SignupPage2';
+import Navbar from './components/navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/signup2" element={<SignupPage2 />} /> {/* New route */}
+      </Routes>
+    </Router>
   );
 }
 
